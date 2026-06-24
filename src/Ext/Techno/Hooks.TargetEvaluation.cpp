cc1c37fe8e346c7b43da17601356c163ec095f7c
SethGekco <jstudeman24@gmail.com>
2026-06-23 20:04:03 -0700

    Remove Put hook - check Always flag directly in EvaluateObject

--- src/Ext/Techno/Hooks.TargetEvaluation.cpp
+++ src/Ext/Techno/Hooks.TargetEvaluation.cpp
@@ -1,19 +1,22 @@
 #include <HouseClass.h>
 #include <Commands/AggressiveStance.h>
+#include <Ext/TechnoType/Body.h>
 #include <Helpers/Macro.h>
-
-// Cursor & target acquisition stuff not directly tied to other features can go here.
 
 DEFINE_HOOK(0x6F858F, TechnoClass_EvaluateObject_AggressiveStance, 0x7)
 {
-	GET(TechnoClass*, pThis, EDI);
-	GET(TechnoClass*, pTarget, ESI);
-	if (pThis && pThis->Owner->IsControlledByHuman()
-		&& pTarget && pTarget->WhatAmI() == AbstractType::Building)
-	{
-		if (AggressiveStanceClass::AggressiveStanceMap[pThis] || (pThis->Transporter && AggressiveStanceClass::AggressiveStanceMap[pThis->Transporter])) {
-			return 0x6F88BF;
-		}
-	}
-	return 0;
+    GET(TechnoClass*, pThis, EDI);
+    GET(TechnoClass*, pTarget, ESI);
+    if (pThis && pThis->Owner->IsControlledByHuman()
+        && pTarget && pTarget->WhatAmI() == AbstractType::Building)
+    {
+        bool isAggressive = AggressiveStanceClass::AggressiveStanceMap[pThis]
+            || TechnoTypeExt::IsAlwaysAggressiveStance(pThis->GetTechnoType())
+            || (pThis->Transporter && (AggressiveStanceClass::AggressiveStanceMap[pThis->Transporter]
+                || TechnoTypeExt::IsAlwaysAggressiveStance(pThis->Transporter->GetTechnoType())));
+
+        if (isAggressive)
+            return 0x6F88BF;
+    }
+    return 0;
 }
--- src/Ext/TechnoType/Body.cpp
+++ src/Ext/TechnoType/Body.cpp
@@ -1,11 +1,7 @@
 #include "Body.h"
 
-#include <Helpers/Macro.h>
-#include <TechnoClass.h>
 #include <TechnoTypeClass.h>
 #include <CCINIClass.h>
-#include <RulesClass.h>
-#include <Commands/AggressiveStance.h>
 
 std::map<TechnoTypeClass*, bool> TechnoTypeExt::AggressiveStanceAlwaysMap;
 
@@ -15,7 +11,7 @@ bool TechnoTypeExt::IsAlwaysAggressiveStance(TechnoTypeClass* pType)
     if (it != TechnoTypeExt::AggressiveStanceAlwaysMap.end())
         return it->second;
 
-    // Not cached yet - read from RulesIni now and cache it
+    // Lazy read from rules INI on first encounter
     CCINIClass* pINI = CCINIClass::INI_Rules;
     if (pINI && pType && pType->ID)
     {
@@ -26,26 +22,4 @@ bool TechnoTypeExt::IsAlwaysAggressiveStance(TechnoTypeClass* pType)
 
     TechnoTypeExt::AggressiveStanceAlwaysMap[pType] = false;
     return false;
-}
-
-// Hook: TechnoClass::Put (0x70D0D0)
-// Fires when a unit is placed on the map.
-// ESI = TechnoClass* (thiscall via PUSH ESI; MOV ESI,ECX pattern - ECX = this)
-// Actually: 53 56 8B F1 = PUSH EBX; PUSH ESI; MOV ESI,ECX so ECX has this at entry
-// We steal 6 bytes: 53 56 8B F1 57 8D
-
-DEFINE_HOOK(0x70D0D0, TechnoClass_Put_AggressiveStanceAlways, 0x6)
-{
-    GET(TechnoClass*, pThis, ECX);
-
-    if (pThis)
-    {
-        TechnoTypeClass* pType = pThis->GetTechnoType();
-        if (pType && TechnoTypeExt::IsAlwaysAggressiveStance(pType))
-        {
-            AggressiveStanceClass::AggressiveStanceMap[pThis] = true;
-        }
-    }
-
-    return 0;
 }
